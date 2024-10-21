086501dac0527885d9dd705c115f747ed919c2dd
Kwonyougsu <167047369+Kwonyougsu@users.noreply.github.com>
2024-10-21 20:52:15 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.24 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int solution(int n) {
+        int count = 0;
+
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                count++; 
+                if (i != n / i) {
+                    count++; 
+                }
+            }
+        }
+
+        return count;
+    }
+}
