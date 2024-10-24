84523b5f097e632644aa2bd1a699e1df8a62ad0e
Kwonyougsu <167047369+Kwonyougsu@users.noreply.github.com>
2024-10-24 19:34:31 +0900

    [level 0] Title: 개미 군단, Time: 0.24 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        for(int i = 0; i < hp; i++)
+        {
+            int G = hp / 5;
+            int H = (hp - G*5) / 3 ;
+            int C = (hp - G*5 - H*3) / 1;
+            
+            answer = G+H+C;
+        }
+        return answer;
+    }
+}
+//장군개미 5, 병정개미 3, 일개미1
+//hp값에 따라 장군,병정,일개미를 정한다. => hp를 개미의 공격력에 따라 나누기 
