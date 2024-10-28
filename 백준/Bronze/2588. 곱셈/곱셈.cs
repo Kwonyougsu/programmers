dd41f46d81d8aceeff107c0f05313f23996fd201
Kwonyougsu <167047369+Kwonyougsu@users.noreply.github.com>
2024-10-28 17:57:15 +0900

    [Bronze III] Title: 곱셈, Time: 48 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cs
@@ -0,0 +1,7 @@
+int a = int.Parse(Console.ReadLine());
+String b = Console.ReadLine();
+
+Console.WriteLine(a * int.Parse(b[2].ToString()));
+Console.WriteLine(a * int.Parse(b[1].ToString()));
+Console.WriteLine(a * int.Parse(b[0].ToString()));
+Console.WriteLine(a * int.Parse(b));
