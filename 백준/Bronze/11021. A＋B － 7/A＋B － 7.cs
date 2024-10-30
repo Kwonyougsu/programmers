bc3a5196e0ff25d9f4225a92e4bda4aabadc72d4
Kwonyougsu <167047369+Kwonyougsu@users.noreply.github.com>
2024-10-30 18:13:41 +0900

    [Bronze V] Title: A+B - 7, Time: 56 ms, Memory: 6008 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cs
@@ -0,0 +1,9 @@
+int T = int.Parse(Console.ReadLine());
+
+for(int i =1; i<= T; i++)
+{
+    String[] N = Console.ReadLine().Split(' ');
+    int A = int.Parse(N[0]);
+    int B = int.Parse(N[1]);
+    Console.WriteLine($"Case #{i}: {A+B}");
+}
