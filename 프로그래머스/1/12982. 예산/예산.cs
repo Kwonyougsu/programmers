3044a74252b82a8be4674b270668f71c0cf19fc3
Kwonyougsu <167047369+Kwonyougsu@users.noreply.github.com>
2024-11-06 17:28:53 +0900

    [level 1] Title: 예산, Time: 2.40 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.cs
+++ 프로그래머스/1/12982. 예산/예산.cs
@@ -2,19 +2,22 @@ using System;
 
 public class Solution
 {
-        public int solution(int[] d, int budget)
+    public int solution(int[] d, int budget)
+    {
+        int answer = 0;
+        Array.Sort(d);
+        for(int i =0; i< d.Length; i++)
         {
-            int answer = 0;	
-            Array.Sort(d);
-            for(int i = 0; i < d.Length; ++i)
+            if(budget - d[i] >= 0)
             {
-                if(budget >= d[i])
-                {
-                    budget -= d[i];
-                    answer++;
-                }
+                budget -= d[i];
+                answer++;
             }
-
-            return answer;
+            else
+                return answer;
         }
+        
+        return answer;
+    }
 }
+//S사 예산, 신청한 금액보다 적은금액은 불가능, 신청금액 배열 D, 예산 budget , 최대 몇?
